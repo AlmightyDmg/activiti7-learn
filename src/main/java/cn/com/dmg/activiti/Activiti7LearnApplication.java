85f3d3ffd1980ea4af0bc13bcba43d4d8f5cbd72
zhum <zhumeng@skyvis.com.cn>
2022-02-11 15:04:33 +0800

    Initial commit

---
+++ src/main/java/cn/com/dmg/activiti/Activiti7LearnApplication.java
@@ -0,0 +1,20 @@
+package cn.com.dmg.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @author zhum
+ * @date 2022/2/11 15:04
+ * @param null
+ * @Return 
+ */
+@SpringBootApplication
+public class Activiti7LearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Activiti7LearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/com/dmg/activiti/Activiti7LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.dmg.activiti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activiti7LearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
